e3a08a0c7594f952b87dc77a8a6ed41f516b28e9
Xochitl <xocaid@gmail.com>
2024-08-16 14:42:06 -0700

    addtl code for edit thread

--- menus/threads/edit_thread.py
+++ menus/threads/edit_thread.py
@@ -26,7 +26,9 @@ def edit_thread():
 
     user_input = user_input_int(len(empty_thread_list))
 
-    print('What aspect of this Thread would you like to edit?')
+
+# Select Thread aspect to edit.
+    print('What Thread info would you like to edit?')
 # Keys that are not editable by user.
     block_list = ['alternatives']
 
@@ -36,35 +38,36 @@ def edit_thread():
          if key in block_list:
               continue
          print(index + 1, '.', key.title())
-    
-    user_input = user_input_int(len(empty_thread_list[user_input-1]))
-    selected_thread_key = thread_keys[user_input - 1]
+
+# Selected Thread Key to edit.
+    thread_key_user_input = user_input_int(len(empty_thread_list[user_input-1]))
+    selected_thread_key = thread_keys[thread_key_user_input - 1]
+    #### For debugging purposes, can be deleted after 
     print(selected_thread_key, "Line 40")
 
-    print("You are editing the Thread color: ", empty_thread_list[user_input-1])
-    edited_thread = (input("Please enter the new thread color.")).title()
+    print("You are editing the Thread", selected_thread_key.title(), ".")
+    edited_thread = (input(f"Please enter the new {selected_thread_key}: "))
 
     if edited_thread.strip() == "":
         print('No edits were made.')
         ###### Needs to back to the brand menu at the beginning
-    you_sure = input("Are you satisfied with the edits?")
+    confirm_edit = input(f"New Edit: {edited_thread.title()}. Are these edits correct?")
         
-    if you_sure.upper() == 'Y' or you_sure.upper() =='YES':
-        empty_thread_list[user_input-1] = edited_thread
+    if confirm_edit.upper() == 'Y' or confirm_edit.upper() =='YES':
+        empty_thread_list[user_input-1][selected_thread_key] = edited_thread
         print('You successfully changed the thread to: ', edited_thread)
-    elif you_sure.upper() == 'N' or you_sure.upper() == 'NO':
+    elif confirm_edit.upper() == 'N' or confirm_edit.upper() == 'NO':
         ###### Need to go back to edited_thread, line 28
         print('Start over, edit the thread name:')
 
     # For debugging purposes, can be deleted after 
     print('Printing from EDIT Thread', empty_thread_list)
 
-
-# EDIT
-# if number or symbols ONLY --> do not save
-
 # NEED TO ADD:
+# if number or symbols ONLY --> do not save
 # will need to go back to the beginning, the brand/thread list
 # will need to go back to the edited_thread to edit again
 # need to be able to exit the edit menu and go back at any time
 # will need to edit thread by : name, length, etc.
+
+# YES --> use a while loop --> when you reach the bottom/end, go back & start over
