5045defd58d5ca787edcfaa5c82f3547c61e1356
xocaid <104172364+xocaid@users.noreply.github.com>
2025-03-07 15:11:43 -0800

    Updated main menu to include option to exit program

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from menus.brands.manage_brands_menu import manage_brands_menu
 from menus.threads.manage_threads_menu import manage_threads_menu
 from menus.utilities import user_input_int
+import sys
 
 def main():
     print('What action do you want to perform?')
 
     print('1. Manage Brands')
     print('2. Manage Threads')
+    print('3. Would you like to exit the program?')
 
-    user_input = user_input_int(2)
+    user_input = user_input_int(3)
 
     if user_input == 1:
         manage_brands_menu()
     elif user_input == 2:
         manage_threads_menu()
     else:
-        print('See you next time.')
+        print('You are exiting the program.')
+        print('See you next time!')
+        sys.exit(0)
 
 main()
