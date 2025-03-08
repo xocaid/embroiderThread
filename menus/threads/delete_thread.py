434bd47f5eef8d5c8a657ea7c69889ba0e975111
xocaid <104172364+xocaid@users.noreply.github.com>
2025-03-07 17:38:55 -0800

    Delete Thread logic

--- menus/threads/delete_thread.py
+++ menus/threads/delete_thread.py
@@ -1,2 +1,25 @@
+from menus.utilities import user_input_int
+from embroidery_thread import emb_thread
+
+## Main function: To delete thread from the database
 def delete_thread():
-    print('delete thread')
+    print('Delete Thread from Inventory')
+    print('What thread would you like to delete?')
+    for x in range(0, len(emb_thread['threads'])):
+        print(x+1,'.', emb_thread['threads'][x]['name'])
+
+    user_input = user_input_int(len(emb_thread['threads'])) #5
+    # call pop method & save to variable
+    deleted_thread = emb_thread['threads'].pop(user_input-1)
+    #print statement with pop value
+    print(f'Successfully deleted the color: {deleted_thread['name']}')
+
+## Delete another thread
+def loop_delete_thread():
+    while True:
+        delete_thread()
+        user_input =input("Would you like to delete another thread?")
+        if user_input.lower() == "yes" or user_input.lower() == "y":
+            continue
+        else:
+            break
--- menus/threads/manage_threads_menu.py
+++ menus/threads/manage_threads_menu.py
@@ -2,24 +2,30 @@ from menus.utilities import user_input_int
 from menus.threads.list_thread_menu import list_thread_menu
 from menus.threads.create_thread import create_thread_menu
 from menus.threads.edit_thread import edit_thread
-from menus.threads.delete_thread import delete_thread
+from menus.threads.delete_thread import loop_delete_thread
+from menus.utilities import which_menu
+from menus.utilities import crud_ops
+from menus.utilities import loop_crud_ops
 
 def manage_threads_menu():
     print('Welcome to the Threads menu.')
     print('What would you like to do with the Threads menu?')
 
-    print('1. List Threads')
-    print('2. Create New Thread')
-    print('3. Edit Thread')
-    print('4. Delete Thread')
+# Store the array to reuse list items for which_menu()
+    menu_options = crud_ops('Threads')
+    loop_crud_ops(menu_options)
 
     user_input = user_input_int(4)
-
     if user_input == 1:
-        list_thread_menu()
+        menu_fxn_to_call = list_thread_menu
     elif user_input ==2:
-        create_thread_menu()
+        menu_fxn_to_call =create_thread_menu
     elif user_input ==3:
-        edit_thread()
-    else:
-        delete_thread()
+        menu_fxn_to_call =edit_thread
+    elif user_input ==4:
+        menu_fxn_to_call =loop_delete_thread
+        print('From: Manage_Threads_Menu: Loop delete thread')
+        
+    menu_fxn_to_call()
+
+    which_menu(menu_fxn_to_call,manage_threads_menu, menu_options[user_input - 1], 'Threads')
